ba934232968817cdbe6bfdd3224fe09aeeecd353
Mel Almada <101682418+melxtear@users.noreply.github.com>
2022-09-27 20:14:11 -0300

    Arreglos en unit test
    Sacamos pruebas que fallaban si o si

--- unit-test/example.cpp
+++ unit-test/example.cpp
@@ -29,14 +29,6 @@ namespace foobar::tests
 		foobar::Example esPar{};
 		ASSERT_THAT(esPar.esPar(3), false);//pasa la prueba, recibe un numero impar por lo tanto devuelve false correctamente
 	}
-	TEST(esPar5, NoPar5) {
-		foobar::Example esPar{};
-		ASSERT_THAT(esPar.esPar(13), true);//no pasa la prueba, recibe un numero impar pero en vez de devolver false devuelve true como si fuera un numero par (lo cual es incorrecto)
-	}
-	/*TEST(esPar6, NoPar6) {
-		foobar::Example esPar{};
-		ASSERT_THAT(esPar.esPar('D'), false);//probamos con que reciba una letra en vez de un nro pero siempre va a ser false ya que no entra a la condición de if sino a la de else en la cual todo es falso, CONSULTAR
-	}*/
 
 	TEST(esVocal1, Vocal1) {
 		foobar::Example esVocal{};
@@ -54,15 +46,6 @@ namespace foobar::tests
 		foobar::Example esVocal{};
 		ASSERT_THAT(esVocal.esVocal('H'), false);//pasa la prueba, recibe una consonante en mayuscula y por lo tanto devuelve false correctamente
 	}
-	TEST(esVocal5, NoVocal5) {
-		foobar::Example esVocal{};
-		ASSERT_THAT(esVocal.esVocal('M'), true);//no pasa la prueba, recibe una consonante en mayuscula pero en vez de devolver false, devuelve true como si fuera una vocal (lo cual es incorrecto)
-	}
-	/*TEST(esVocal6, NoVocal6) {
-		foobar::Example esVocal{};
-		ASSERT_THAT(esVocal.esVocal(1), false);//probamos con que reciba un nro en vez de una letra pero siempre va a ser false ya que no entra a la condición de if sino a la de else en la cual todo es falso, CONSULTAR
-	}*/
 
-	//conclusion, hay 4 pruebas de la función esVocal que son exitosas y una tira error. Y hay 4 pruebas de la funcion esPar exitosas y una no lo es.
-	
+	//conclusion: todas las pruebas están bien
 }
